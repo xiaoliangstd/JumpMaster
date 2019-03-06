d19c02b3044d2e2d6f31ec0416020f3f2afbf1b4
liangwei0 <2303335747@qq.com>
2019-03-06 17:24:10 +0800

    dd

--- main.py
+++ main.py
@@ -11,26 +11,34 @@ ratio = 0.5
 def mouse_val(event,x,y,flags,param):
     global mx,my,che_x,che_y,ratio
     
+    
     if event == cv.EVENT_LBUTTONDOWN:
+        x1 = np.random.randint(550,600)
+        y1 = np.random.randint(350,550)
         time = math.sqrt(abs(che_x - x)**2 + abs(che_y - y)**2)
         print("distance : ",math.sqrt(abs(che_x - x)**2 + abs(che_y - y)**2))
-        cal_time = int(time*2.5)
-        adb.touch(cal_time)
+        cal_time = int(time*2.2)
+        adb.touch(x1,y1,cal_time)
+        
 
 
 
 
 if __name__ == "__main__":
-    
     adb = Adb()
     jmp = jumpmaster()
     while True: 
         img = adb.screenshot()
         canvas = np.copy(img)  
-        canvas,che_x,che_y = jmp.findChess(img,canvas)
+        try:
+            canvas,che_x,che_y = jmp.findChess(img,canvas)
+        except TypeError :
+            continue
+        
         cv.imshow("liang.png",canvas)
         cv.setMouseCallback("liang.png",mouse_val)
-        cv.waitKey(500)
+        cv.waitKey(800)
+        
 
         
             
