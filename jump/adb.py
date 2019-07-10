42ae332968d3a01b288a928739c77627f93891a3
liangwei0 <2303335747@qq.com>
2019-07-10 14:05:12 +0800

    dd

---
+++ jump/adb.py
@@ -0,0 +1,43 @@
+'''
+author : liangwei
+
+'''
+import subprocess as sub 
+import shlex 
+import cv2 as cv 
+import numpy as np 
+import math
+
+
+class Adb:
+
+    def __init__(self):
+        self.img = None
+
+    def screenshot(self):
+        ''' get an image from android phone'''
+        sub.call("adb shell screencap -p | sed 's/\r$//' > liang.png",shell = True)
+        img = cv.imread("liang.png",1)
+        cv.imshow("img",img)
+        resize = cv.resize(img,(700,900))
+        self.img = resize
+        return self.img
+
+    def againtencent(self):
+        pass
+
+    def touch(self,che_x,che_y,x,y):
+        x1 = np.random.randint(550,600)
+        y1 = np.random.randint(350,550)
+        time = math.sqrt(abs(che_x - y)**2 + abs(che_y - (x+40))**2)
+        cal_time = int(time*2.2)
+        sub.call("adb shell input touchscreen swipe {} {} {} {} {}".format(x1,y1,x1+20,y1+20,cal_time),shell = True)
+
+
+
+if __name__ == "__main__":
+    adb = Adb()
+    adb.screenshot()
+    adb.againtencent()
+    cv.waitKey(0)
+    cv.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,38 @@
+from jump.adb import Adb
+from jump.process import jumpmaster 
+import cv2 as cv 
+import numpy as np 
+import math
+import time
+
+mx = 0
+my = 0
+ratio = 0.5
+def mouse_val(event,x,y,flags,param):
+    global mx,my,che_x,che_y,ratio
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        x1 = np.random.randint(550,600)
+        y1 = np.random.randint(350,550)
+        time = math.sqrt(abs(che_x - x)**2 + abs(che_y - y)**2)
+        #print("distance : ",math.sqrt(abs(che_x - x)**2 + abs(che_y - y)**2))
+        cal_time = int(time*2.2)
+        adb.touch(x1,y1,cal_time)
+
+if __name__ == "__main__":
+    adb = Adb()
+    jmp = jumpmaster()
+    while True: 
+        img = adb.screenshot()   # 发送ADB指令截图 
+        try:
+            che_x,che_y = jmp.findChess(img)
+            x,y = jmp.findBox()
+            jmp.visual()
+            cv.waitKey(500)
+            adb.touch(che_x,che_y,x,y)
+            
+        except TypeError :
+            continue 
+        cv.waitKey(800)    
+        
+    cv.destroyAllWindows()
